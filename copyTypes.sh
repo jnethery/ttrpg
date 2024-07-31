1184b3558bebddd4d6807f2d2caf2790dab1caf4
Josiah Nethery <1271885+jnethery@users.noreply.github.com>
2024-07-31 17:20:01 -0400

    Updating copyTypes script

--- copyTypes.sh
+++ copyTypes.sh
@@ -1,4 +1,4 @@
 # This is a quick and dirty script to copy the types from the server to the client
 
 # Copy the types from the server to the client
-cp -r ttrpg-server/src/types ttrpg-client/src
+rsync -av ttrpg-server/src/types ttrpg-client/src
