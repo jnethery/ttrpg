66da75e6b27264d28c0b669c9419cc603a1dbf57
Josiah Nethery <1271885+jnethery@users.noreply.github.com>
2024-08-02 16:13:39 -0400

    Fixing type copy script

--- copyTypes.sh
+++ copyTypes.sh
@@ -1,4 +1,4 @@
 # This is a quick and dirty script to copy the types from the server to the client
 
 # Copy the types from the server to the client
-rsync -av ttrpg-server/src/types ttrpg-client/src
+cp -r ttrpg-server/src/types ttrpg-client/src
