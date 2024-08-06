1b383252b4bdff15eacd5f3f888080b83a059b47
Josiah Nethery <1271885+jnethery@users.noreply.github.com>
2024-08-06 17:24:55 -0400

    Adding brush settings

--- ttrpg-client/src/components/Toolbar/InspectorViews/BrushSettingsInspector.tsx
+++ ttrpg-client/src/components/Toolbar/InspectorViews/BrushSettingsInspector.tsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import { Container, Slider, Typography } from '@mui/material'
 
+import { useToolContext } from 'hooks/useToolContext'
 import { Panel } from 'components/Layout'
 
 export const BrushSettingsInspector: React.FC = () => {
-  return <Panel elevation={2}>Ok</Panel>
+  const { brushSettings, setBrushSettings } = useToolContext()
+
+  return (
+    <Panel elevation={2}>
+      <Container style={{ padding: 5 }}>
+        <Typography id="input-slider" gutterBottom>
+          Brush Size ({brushSettings.size} tile
+          {brushSettings.size === 1 ? '' : 's'})
+        </Typography>
+        <Slider
+          style={{
+            width: '90%',
+          }}
+          aria-labelledby="input-slider"
+          value={brushSettings.size}
+          min={1}
+          max={50}
+          onChange={(_, value) => {
+            setBrushSettings((prev) => ({
+              ...prev,
+              size: value as number,
+            }))
+          }}
+        />
+      </Container>
+    </Panel>
+  )
 }
